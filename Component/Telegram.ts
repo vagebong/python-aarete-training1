f0135cc1c4d2dbf976615b11075582b1707823a0
vagebong <122693284+vagebong@users.noreply.github.com>
2020-08-20 14:41:15 +0000

    msm: vidc: Add min resolution check for video session

--- Component/Telegram.ts
+++ Component/Telegram.ts
@@ -23,3 +23,118 @@ export class Telegram {
     private list: ListManager;
     private bot: TelegramBot;
     private me!: User;
+    private messageQueue = new Queue(1);
+    private audioAddSession = new Map<number, ObjectId>();
+
+    constructor(core: Core) {
+        if (!core.config.telegram.token) throw ERR_MISSING_TOKEN;
+
+        this.user = core.userManager;
+        this.audio = core.audioManager;
+        this.list = core.listManager;
+
+        // Create bot
+        this.bot = new TelegramBot(core.config.telegram.token as string, {
+            polling: true,
+        });
+
+        // Register URLParser
+        this.audio.urlParser.registerURLHandler("^tg://", this.getFile.bind(this));
+        this.audio.urlParser.registerMetadataProvider("^tg://", this.getMetadata.bind(this));
+
+        // Register listener
+        void this.bot.getMe().then(me => {
+            this.me = me ;
+            this.listener();
+        });
+    }
+
+    private listener() {
+        // Handle command
+        this.bot.onText(/^\/(\w+)@?(\w*)/i, (msg, match) => {
+            if (!match || msg.chat.type !== "private" && match[2] !== this.me.username) return;
+            switch (match[1]) {
+                case "register":
+                    void this.commandRegister(msg);
+                    break;
+                case "bind":
+                    void this.commandBind(msg);
+                    break;
+                case "info":
+                    void this.commandInfo(msg);
+                    break;
+                case "list":
+                    void this.commandShowList(msg);
+                    break;
+            }
+        });
+
+        // Audio
+        this.bot.on("audio", async (msg: Message) => {
+            await this.checkSessionPermission(msg);
+            void this.processAudio(msg);
+        });
+
+        // File
+        this.bot.on("document", async (msg: Message) => {
+            await this.checkSessionPermission(msg);
+            void this.processFile(msg);
+        });
+
+        // Link
+        this.bot.on("text", async (msg: Message) => {
+            if (msg.entities && msg.entities.some(entity => entity.type.match(/url|text_link/ig) != null)) {
+                await this.checkSessionPermission(msg);
+                void this.sendProcessing(msg);
+                for (const entity of msg.entities) {
+                    if (entity.type === "url" && msg.text) {
+                        void this.processLink(msg, msg.text.substr(entity.offset, entity.length));
+                    }
+
+                    if (entity.type === "text_link" && entity.url) {
+                        void this.processLink(msg, entity.url);
+                    }
+                }
+            }
+        });
+
+        // Audio ID
+        this.bot.onText(/^([0-9a-f]{24})$/i, async (msg, match) => {
+            await this.checkSessionPermission(msg);
+            const session = this.audioAddSession.get(msg.chat.id);
+            if (!session || !match) return;
+
+            const audio = await this.audio.get(new ObjectId(match[1]));
+            if (!audio) {
+                void this.queueSendMessage(msg.chat.id, "Sound ID not found in database", { reply_to_message_id: msg.message_id });
+                return;
+            }
+
+            await this.list.addAudio(session, audio._id);
+            void this.queueSendMessage(msg.chat.id, "Added to list!", { reply_to_message_id: msg.message_id });
+        });
+
+        // Inline button
+        this.bot.on("callback_query", async (query: CallbackQuery) => {
+            void this.bot.answerCallbackQuery(query.id);
+
+            if (!query.data) return;
+            const data = query.data.split(" ");
+
+            switch (data[0]) {
+                case "AudioInfo":
+                    await this.audioInfoCallback(query, data);
+                    break;
+                case "List":
+                    await this.playlistCallback(query, data);
+                    break;
+                case "ListInfo":
+                    await this.listInfoCallback(query, data);
+                    break;
+                case "ListCreate":
+                    await this.listCreateCallback(query, data);
+                    break;
+                case "ListAudioAdd":
+                    await this.listAudioAddCallback(query, data);
+                    break;
+                case "ListAudioDel":
