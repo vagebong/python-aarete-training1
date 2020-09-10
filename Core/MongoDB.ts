21ef962e84c6022834533743e9aa28b6704d1012
vagebong <122693284+vagebong@users.noreply.github.com>
2020-09-10 06:41:40 +0000

    Update awesome-angular2.md

--- Core/MongoDB.ts
+++ Core/MongoDB.ts
@@ -1,2 +1,6 @@
 import { EventEmitter } from "events";
-import { Db, MongoClien
+import { Db, MongoClient } from "mongodb";
+
+export const ERR_DB_NOT_INIT = Error("Database is not initialized");
+
+// tslint:disable-next-line:interf
