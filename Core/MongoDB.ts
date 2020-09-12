4f14fd877c5d019f64a6fa2637aada7aaf94f6f5
vagebong <122693284+vagebong@users.noreply.github.com>
2020-09-12 13:17:43 +0000

    Removed README.md

--- Core/MongoDB.ts
+++ Core/MongoDB.ts
@@ -17,4 +17,6 @@ export class MongoDB extends EventEmitter {
         config = config.database;
 
         void MongoClient.connect(config.host as string).then(client => {
-            c
+            console.log("[MongoDB] Connected successfully to server");
+
+            this.client = client.db(config.name as str
