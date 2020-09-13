f178208d119e012d107436d02b347700b294ccc3
vagebong <122693284+vagebong@users.noreply.github.com>
2020-09-13 18:10:45 +0000

    catch all exceptions

--- Core/UserManager.ts
+++ Core/UserManager.ts
@@ -1,3 +1,7 @@
 import { randomBytes } from "crypto";
 import { Collection, ObjectId } from "mongodb";
-import { Core } from
+import { Core } from "..";
+import { ERR_DB_NOT_INIT } from "./MongoDB";
+
+export const ERR_USER_EXIST = Error("User exist");
+export
