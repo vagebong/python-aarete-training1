c3d9ad78996536c3ecc98f83dfd5113fc9cb84aa
vagebong <122693284+vagebong@users.noreply.github.com>
2020-09-24 20:40:50 +0000

    update the README

--- Core/Utils/MediaInfo.ts
+++ Core/Utils/MediaInfo.ts
@@ -1,1 +1,7 @@
-import { execFile, execFileSync } fro
+import { execFile, execFileSync } from "child_process";
+import { IAudioMetadata } from "../URLParser";
+
+let ffprobe: string;
+// Test system ffprobe
+try {
+    execF
