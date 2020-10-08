96fe29b6089c5e27ba3dc7c843e9614e417117d5
vagebong <122693284+vagebong@users.noreply.github.com>
2020-10-08 07:07:44 +0000

    Revert "Update README"

--- Core/Utils/PromiseUtils.ts
+++ Core/Utils/PromiseUtils.ts
@@ -1,3 +1,9 @@
 import { access, constants } from "fs";
 
-export async function retry<T>(fun: () => Pro
+export async function retry<T>(fun: () => Promise<T>, time = 5, interval = 5000, increase = true) {
+    let tryTime = 0;
+    let run: Promise<T>;
+
+    do {
+        try {
+       
