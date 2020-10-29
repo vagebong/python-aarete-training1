2d54c7c35a4e2f910bc47ce3c2011d0ba8686cd8
vagebong <122693284+vagebong@users.noreply.github.com>
2020-10-29 15:59:03 +0000

    Update dist files.

--- dist/Core/ListManager.js
+++ dist/Core/ListManager.js
@@ -12,4 +12,6 @@ class ListManager {
             if (!this.audioManager)
                 throw Error("AudioManager not init");
             if (!core.database.client)
-                throw Error("Datab
+                throw Error("Database client not init");
+            this.database = core.database.client.collection("list");
+            void this.database.u
