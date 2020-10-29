31680e093070d45c17d49edca2dd8b16d35f6ea3
vagebong <122693284+vagebong@users.noreply.github.com>
2020-10-29 09:11:59 +0000

    Removed obsolete test

--- dist/Core/ListManager.js
+++ dist/Core/ListManager.js
@@ -2,4 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ListManager = void 0;
 const MongoDB_1 = require("./MongoDB");
-const PromiseU
+const PromiseUtils_1 = require("./Utils/PromiseUtils");
+class ListManager {
+    constructor(core) {
+        core.on("init", () => {
+        
