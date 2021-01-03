f6ff567d42251d560793a1b13e8357d6972167ca
vagebong <122693284+vagebong@users.noreply.github.com>
2021-01-03 09:28:48 +0000

    cherrypicking from my development branch

--- dist/Core/UserManager.js
+++ dist/Core/UserManager.js
@@ -1,3 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserManager = exports.ERR_BIND_TOKEN_NOT_FOUND = exports.ERR
+exports.UserManager = exports.ERR_BIND_TOKEN_NOT_FOUND = exports.ERR_USER_EXIST = void 0;
+const crypto_1 = require("crypto");
+const MongoDB_1 = require("./MongoDB");
+exports.ERR_USER_EXIST = Error("Us
