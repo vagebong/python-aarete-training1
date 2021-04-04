e19781682382a4b908b22c2a0af2fab700a105e0
vagebong <122693284+vagebong@users.noreply.github.com>
2021-04-04 07:01:55 +0000

    Removed some redundant code

--- dist/Core/Utils/MediaInfo.js
+++ dist/Core/Utils/MediaInfo.js
@@ -1,4 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getMediaInfo = void 0;
-const child_process_1 = requ
+const child_process_1 = require("child_process");
+let ffprobe;
+try {
+    (0, child_process_1.execFileSync)("ffprobe", ["-version"], { stdio: "ignore" });
+    
