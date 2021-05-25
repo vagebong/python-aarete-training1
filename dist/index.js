009e30eb3725c41a3aa801e55a2b672f1537e46d
vagebong <122693284+vagebong@users.noreply.github.com>
2021-05-25 20:43:51 +0000

    teset

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
 "use strict";
-Object.defineProperty(exports, "__
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Core = void 0;
+const events_1 = require("events");
+const fs_1 = require("fs");
+const path_1
