1299e3a2ec8ae7e6a5df2d65f200d0e45b93fb27
vagebong <122693284+vagebong@users.noreply.github.com>
2021-06-08 08:20:47 +0000

    frontend join

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,7 @@ const events_1 = require("events");
 const fs_1 = require("fs");
 const path_1 = require("path");
 const Discord_1 = require("./Component/Discord");
-const Telegram_1 = require("./Component/Tele
+const Telegram_1 = require("./Component/Telegram");
+const AudioManager_1 = require("./Core/AudioManager");
+const ListManager_1 = require("./Core/ListManager");
+co
