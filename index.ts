73432daa0d0b339327aeb5949bbd55857eb4ac3b
vagebong <122693284+vagebong@users.noreply.github.com>
2022-04-27 06:36:08 +0000

    Initial refactor

--- index.ts
+++ index.ts
@@ -3,4 +3,7 @@ import { existsSync, mkdirSync } from "fs";
 import { resolve } from "path";
 import { Discord } from "./Component/Discord";
 import { Telegram } from "./Component/Telegram";
-import { AudioManager } from "./Core/AudioM
+import { AudioManager } from "./Core/AudioManager";
+import { ListManager } from "./Core/ListManager";
+import { MongoDB } from "./Core/MongoDB";
+import { U
